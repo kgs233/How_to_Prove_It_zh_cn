7b0e4e3af28a8aab3a508008602a868310fbb00c
kgs233 <kgs233@126.com>
2025-08-31 01:58:41 +0800

    add latex2typst

---
+++ latex2typst.ps1
@@ -0,0 +1,29 @@
+$files = Get-ChildItem -Path ./ -Recurse -File | Where-Object { $_.Name -ne "latex2typst.ps1" } | Where-Object { $_.Name -ne "ref.md" } | Where-Object { $_.Name -ne "ref.pdf" }
+
+foreach ($file in $files) {
+    $content = Get-Content -Path $file.FullName
+
+    $content = $content -replace '\\in', 'in'
+    $content = $content -replace '\\notin', 'in.not'
+
+    $content = $content -replace '\\lor', 'or'
+    $content = $content -replace '\\land', 'and'
+    $content = $content -replace '\\lnot', 'not'
+
+    $content = $content -replace '\\cap', 'inter'
+    $content = $content -replace '\\cup', 'union'
+    $content = $content -replace '\\setminus', 'without'
+
+    $content = $content -replace '\\emptyset', 'emptyset'
+
+    $content = $content -replace '\\mid', '|'
+    $content = $content -replace '……', 'dots'
+
+    $content = $content -replace '\\{', '{'
+    $content = $content -replace '\\}', '}'
+
+    $content = $content -replace '\\mathbb{([A-Za-z])}', '$1$1'
+    $content = $content -replace '\\text\{([^\}]*)\}', '"$1"'
+
+    Set-Content -Path $file.FullName -Value $content
+}
